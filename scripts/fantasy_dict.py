d4f7e6d1743e0114b2d936d7e15a9804d8dca1ed
Thomas Kalnik <kalnik.thomas@gmail.com>
2018-09-04 00:53:27 -0500

    Delete players

--- scripts/fantasy_dict.py
+++ scripts/fantasy_dict.py
@@ -31,7 +31,33 @@ print("Kickers", '\n', Kdict, '\n')
 DSTdict = DST.set_index('Player').to_dict(orient='dict')
 print("DSTs", '\n', DSTdict)
 
-
+count = 583
+try:
+	while count > 0:
+		a = input("Enter the name of the player that was selected: ")
+		if a in QBdict:
+			del QBdict[a]
+			print(QBdict)
+		elif a in RBdict:
+			del RBdict[a]
+			print(RBdict)
+		elif a in WRdict:
+			del WRdict[a]
+			print(WRdict)
+		elif a in TEdict:
+			del TEdict[a]
+			print(TEdict)
+		elif a in Kdict:
+			del Kdict[a]
+			print(Kdict)
+		elif a in DSTdict:
+			del DSTdict[a]
+			print(DSTdict)
+		else:
+			pass
+	count -= 1
+except KeyboardInterrupt:
+	pass
 
 
 
